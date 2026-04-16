a68315ea14d786a3e5b773385644d8fcc1d8fe86
Choykz <chakib.salhi07@gmail.com>
2026-04-16 09:03:52 +0100

    update

--- hypr/firsttime.sh
+++ hypr/firsttime.sh
@@ -1,8 +1,8 @@
   GNU nano 8.6                                                        ./test.sh                                                                     
 if [ -f "$HOME/.config/hypr/deleteme" ]; then
     echo "The file exists, its first time!"
-    swww-daemon & disown # if not is already running
-    swww img $HOME/.config/hypr/wallpaper.jpeg
+    awww-daemon & disown # if not is already running
+    awww img $HOME/.config/hypr/wallpaper.jpeg
     rm $HOME/.config/hypr/deleteme # delete the file to prevent this from running again
 else
     echo "the file does not exist, all right here"
